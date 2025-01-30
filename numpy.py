28e8776ae6d45f6defefe52ef669ac3d0d76150b
Keshav Gupta <playzbuchika@gmail.com>
2025-01-30 20:51:21 +0530

    Create numpy.py

---
+++ numpy.py
@@ -0,0 +1,20 @@
+data = np.random.rand(2,3,4)
+zeroes = np.zeros((2,2,2))
+full = np.full((2,2,2), 7)
+ones = np.ones((2,2,2))
+arr = np.array([[1,2,3,4],[1,2,3,4]])
+arr = data[0]
+slicer = data[0][0:2]
+reverse = data[-1]
+singleval = data[0][0][0]
+sqrt = np.sqrt(25)
+ab = np.abs(-2)
+power = np.power(2,7)
+log = np.log(25)
+exp = np.exp([2,3])
+mins = np.min(list1)
+maxs = np.max(list1)
+maxs
+
+
+test = np.load("new array.npy")
